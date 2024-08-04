27893953bbb9d42b0779e4f95c94a6822b21a520
omgupta-04 <o.gupta@iitg.ac.in>
2024-08-05 01:52:16 +0530

    Pushing

---
+++ lib/widgets/grocery_list.dart
@@ -0,0 +1,169 @@
+import 'package:flutter/material.dart';
+import 'package:shopping_list/data/categories.dart';
+import 'package:shopping_list/models/grocery_item.dart';
+import 'package:shopping_list/widgets/new_item.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class GroceryList extends StatefulWidget {
+  const GroceryList({super.key});
+
+  @override
+  State<GroceryList> createState() => _GroceryListState();
+}
+
+class _GroceryListState extends State<GroceryList> {
+  List<GroceryItem> _groceryItems = [];
+
+  var _isLoading = true;
+  String? _error;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadItems();
+  }
+
+  void _loadItems() async {
+    final url = Uri.https(
+      'firstproject-omgupta-default-rtdb.firebaseio.com',
+      'shopping-list.json',
+    );
+
+    try{
+      final response = await http.get(url);
+      if (response.statusCode >= 400) {
+      setState(() {
+        _error = 'Failed to fetch data. Please try again later !';
+      });
+    }
+
+    if(response.body == 'null'){
+      setState(() {
+        _isLoading = false;
+      });
+      return;
+    }
+    final Map<String, dynamic> listData = json.decode(response.body);
+
+    final List<GroceryItem> loadedItems = [];
+
+    for (final item in listData.entries) {
+      final category = categories.entries
+          .firstWhere(
+              (catItem) => catItem.value.title == item.value['category'])
+          .value;
+      final groceryItem = GroceryItem(
+        id: item.key,
+        name: item.value['name'],
+        quantity: item.value['quantity'],
+        category: category,
+      );
+      loadedItems.add(groceryItem);
+    }
+    setState(() {
+      _groceryItems = loadedItems;
+      _isLoading = false;
+    });
+    }catch(error){
+      setState(() {
+        _error = 'Something went wrong. Please try again later !';
+      });
+    }
+    
+
+    
+  }
+
+  void _additem() async {
+    final newItem = await Navigator.of(context).push<GroceryItem>(
+      MaterialPageRoute(
+        builder: (ctx) => const NewItem(),
+      ),
+    );
+
+    if (newItem == null) {
+      return;
+    } else {
+      setState(() {
+        _groceryItems.add(newItem);
+      });
+    }
+  }
+
+  void _removeItem(GroceryItem item) async {
+
+    final index = _groceryItems.indexOf(item);
+
+    setState(() {
+      _groceryItems.remove(item);
+    });
+
+    final url = Uri.https(
+      'firstproject-omgupta-default-rtdb.firebaseio.com',
+      'shopping-list/${item.id}.json',
+    );
+
+    final response =  await http.delete(url);
+
+    if (response.statusCode >= 400) {
+      setState(() {
+        _groceryItems.insert(index,item);  
+      });
+    } 
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Widget content = const Center(
+      child: Text('No item added yet.'),
+    );
+
+    if (_isLoading) {
+      content = const Center(
+        child: CircularProgressIndicator(),
+      );
+    }
+
+    if (_groceryItems.isNotEmpty) {
+      content = ListView.builder(
+        itemCount: _groceryItems.length,
+        itemBuilder: (context, index) {
+          return Dismissible(
+            key: ValueKey(_groceryItems[index].id),
+            onDismissed: (direction) {
+              _removeItem(_groceryItems[index]);
+            },
+            child: ListTile(
+              title: Text(_groceryItems[index].name),
+              leading: Container(
+                width: 24,
+                height: 24,
+                color: _groceryItems[index].category.color,
+              ),
+              trailing: Text(_groceryItems[index].quantity.toString()),
+            ),
+          );
+        },
+      );
+    }
+
+    if (_error != null) {
+      content = Center(
+        child: Text(_error!),
+      );
+    }
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Your Groceries '),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.add),
+            onPressed: _additem,
+          ),
+        ],
+      ),
+      body: content,
+    );
+  }
+}
